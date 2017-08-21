fe776fe7e2325488a6fc9db2af703a905ac21311
Matt Wigdahl <mlwigdahl@gmail.com>
2017-08-20 21:23:59 -0500

    finishing logon page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,113 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
 import './App.css';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
+import config from './config.js'
 
 class App extends Component {
 
-  handleNavLink = event => {
-    event.preventDefault();
-    this.props.history.push(event.currentTarget.getAttribute('href'));
-  }
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
-              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes />
-      </div>
-    );
-  }
+        this.state = {
+            userToken: null,
+            isLoadingUserToken: true,
+        };
+    }
+
+    async componentDidMount() {
+        const currentUser = this.getCurrentUser();
+
+        if (currentUser === null) {
+            this.setState({ isLoadingUserToken: false });
+            return;
+        }
+
+        try {
+            const userToken = await this.getUserToken(currentUser);
+            this.updateUserToken(userToken);
+        }
+        catch (e) {
+            alert(e);
+        }
+
+        this.setState({ isLoadingUserToken: false });
+    }
+
+    updateUserToken = userToken => {
+        this.setState({ userToken });
+    }
+
+    handleNavLink = event => {
+        event.preventDefault();
+        this.props.history.push(event.currentTarget.getAttribute('href'));
+    }
+
+    handleLogout = event => {
+        const currentUser = this.getCurrentUser();
+
+        if (currentUser !== null) {
+            currentUser.signOut();
+        }
+
+        this.updateUserToken(null);
+        this.props.history.push('/login');
+    }
+
+    getCurrentUser() {
+        const userPool = new CognitoUserPool({
+            UserPoolId: config.cognito.USER_POOL_ID,
+            ClientId: config.cognito.APP_CLIENT_ID
+        });
+        return userPool.getCurrentUser();
+    }
+
+    getUserToken(currentUser) {
+        return new Promise((resolve, reject) => {
+            currentUser.getSession(function(err, session) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(session.getIdToken().getJwtToken());
+            });
+        });
+    }
+
+    render() {
+
+        const childProps = {
+            userToken: this.state.userToken,
+            updateUserToken: this.updateUserToken,
+        }
+
+        return !this.state.isLoadingUserToken &&
+        (
+            <div className="App container">
+                <Navbar fluid collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <Link to="/">Scratch</Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                      <Nav pullRight>
+                          { this.state.userToken
+                              ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                              : [ <RouteNavItem key={1} onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>,
+                                  <RouteNavItem key={2} onClick={this.handleNavLink} href="/login">Login</RouteNavItem> ] }
+                      </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Routes childProps={childProps} />
+            </div>
+        );
+    }
 }
 
 export default withRouter(App);
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,68 +1,112 @@
 import React, { Component } from 'react';
 import {
-  Button,
-  FormGroup,
-  FormControl,
-  ControlLabel,
+    FormGroup,
+    FormControl,
+    ControlLabel,
 } from 'react-bootstrap';
+import {
+    CognitoUserPool,
+    AuthenticationDetails,
+    CognitoUser
+} from 'amazon-cognito-identity-js';
+import { withRouter } from 'react-router-dom';
+
+import LoaderButton from '../components/LoaderButton'
+
 import './Login.css';
 import config from '../config.js';
 
+
 class Login extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+        this.state = {
+            isLoading: false,
+            username: '',
+            password: '',
+        };
+    }
 
-  validateForm() {
-    return this.state.username.length > 0
-      && this.state.password.length > 0;
-  }
+    validateForm() {
+        return this.state.username.length > 0
+            && this.state.password.length > 0;
+    }
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-  }
+    handleSubmit = async event => {
+        event.preventDefault();
 
-  render() {
-    return (
-      <div className="Login">
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="username" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.username}
-              onChange={this.handleChange} />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password" />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={ ! this.validateForm() }
-            type="submit">
-            Login
-          </Button>
-        </form>
-      </div>
-    );
-  }
+        this.setState({ isLoading: true });
+
+        try {
+            const userToken = await this.login(this.state.username, this.state.password);
+            this.props.updateUserToken(userToken);
+            this.props.history.push('/');
+        }
+        catch(e) {
+            alert(e);
+            this.setState({ isLoading: false });
+        }
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="username" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.username}
+                        onChange={this.handleChange} />
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password" />
+                    </FormGroup>
+                    <LoaderButton
+                        block
+                        bsSize="large"
+                        disabled={ ! this.validateForm() }
+                        type="submit"
+                        isLoading={this.state.isLoading}
+                        text="Login"
+                        loadingText="Logging in..." />
+                </form>
+            </div>
+        );
+    }
+
+    login(username, password) {
+        const userPool = new CognitoUserPool({
+            UserPoolId: config.cognito.USER_POOL_ID,
+            ClientId: config.cognito.APP_CLIENT_ID
+        });
+        const authenticationData = {
+            Username: username,
+            Password: password
+        };
+
+        const user = new CognitoUser({ Username: username, Pool: userPool });
+        const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+        return new Promise((resolve, reject) => (
+            user.authenticateUser(authenticationDetails, {
+                onSuccess: (result) => resolve(result.getIdToken().getJwtToken()),
+                onFailure: (err) => reject(err),
+            })
+        ));
+    }
 }
 
-export default Login;
+export default withRouter(Login);
