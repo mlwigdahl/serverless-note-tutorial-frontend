c2cf553a33cc761f81d5dc78c4132ed358b61f16
Matt Wigdahl <mlwigdahl@gmail.com>
2017-08-20 22:10:44 -0500

    added notes page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 import AppliedRoute from './components/AppliedRoute';
 
@@ -12,6 +13,7 @@ export default ({ childProps }) => (
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
         <AppliedRoute path="/login" exact component={Login} props={childProps}/>
         <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
         <Route component={NotFound} />
     </Switch>
 );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
     USER_POOL_ID : 'us-east-2_JrHhb1kSv',
     APP_CLIENT_ID : '43cdsq2f10vq71letea7kafc5l',
