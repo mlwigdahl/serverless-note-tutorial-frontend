249012db0af55fce6e21571f8292f6e28c1b06d8
Matt Wigdahl <matt@scriptpro.com>
2017-08-22 15:59:17 -0500

    adding IAM request

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+      URL: 'https://pdefrhdywb.execute-api.us-east-2.amazonaws.com/prod',
+      REGION: 'us-east-2',
+  },
   cognito: {
     USER_POOL_ID : 'us-east-2_JrHhb1kSv',
     APP_CLIENT_ID : '43cdsq2f10vq71letea7kafc5l',
+    REGION: 'us-east-2',
+    IDENTITY_POOL_ID: 'us-east-2:81c43cd7-2b52-4ae7-b941-0977dfe55d0b'
   }
 };
